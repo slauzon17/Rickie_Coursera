7ad0d08b029ab6a5fa2fd2beb9b2ceb2e4b7d031
slauzon17 <shelbylauzon17@yahoo.com>
2020-09-16 10:22:36 -0500

    Update of project 2

---
+++ Project2.R
@@ -0,0 +1,59 @@
+path <- getwd()
+
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+              , destfile = paste(path, "dataFiles.zip", sep = "/"))
+unzip(zipfile = "dataFiles.zip")
+
+SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
+NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
+
+## Question 1
+
+# Prevents histogram from printing in scientific notation
+NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
+
+totalNEI <- NEI[, lapply(.SD, sum, na.rm = TRUE), .SDcols = c("Emissions"), by = year]
+
+png(filename='plot1.png')
+
+barplot(totalNEI[, Emissions]
+        , names = totalNEI[, year]
+        , xlab = "Years", ylab = "Emissions"
+        , main = "Emissions over the Years")
+
+dev.off()
+
+
+##Question 2
+
+NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
+totalNEI <- NEI[fips=='24510', lapply(.SD, sum, na.rm = TRUE)
+                , .SDcols = c("Emissions")
+                , by = year]
+
+png(filename='plot2.png')
+
+barplot(totalNEI[, Emissions]
+        , names = totalNEI[, year]
+        , xlab = "Years", ylab = "Emissions"
+        , main = "Emissions over the Years")
+
+dev.off()
+
+##Question 3
+
+# Subset NEI data by Baltimore
+baltimoreNEI <- NEI[fips=="24510",]
+
+png("plot3.png")
+
+ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
+  geom_bar(stat="identity") +
+  facet_grid(.~type,scales = "free",space="free") + 
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
+
+dev.off()
+
+##Question 4
+
